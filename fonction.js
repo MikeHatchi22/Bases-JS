a67521695c6746f8e82cc65e985c498a4624b4d6
Mike <mihatchi@outlook.fr>
2024-03-17 09:03:14 -0400

    All Exercices

--- fonction.js
+++ fonction.js
@@ -1,4 +1,4 @@
-//créer une fonction isPalindrome() qui renverra vrai si un mot est un palindrome et false sinon.
+//créer un tableau avec différents élèves avec leurs notes
 
 const students = [
     {
@@ -23,12 +23,74 @@ const students = [
     }
 ]
 
+//Calculer la moyenne de chaque élève.
+
 const moyenne = function (notes) {
     let sum = 0
     for(let note of notes) {
         sum = sum + note
     }
-    console.log (sum)
+    return sum / notes.length
 }
 
-moyenne([10, 20])
+//Déterminer la moyenne, la pire note et la meilleure note de chaque élève
+
+for(let student of students) {
+    student.moyenne = moyenne(student.notes)
+    student.worst = Math.min(...student.notes)
+    student.meilleur = Math.max(...student.notes)
+}
+
+console.log(students)
+
+//Classer les élèves en fonction de la moyenne la plus haute
+
+const compareStudents = function (a, b) {
+    return b.moyenne - a.moyenne
+}
+
+students.sort(compareStudents)
+
+//Faire un Top 3 des étudiants en fonction des moyennes
+
+const formatStudent = function (student) {
+    return `${student.name} avec une moyenne ${student.moyenne}, meilleure note ${student.meilleur}, pire note ${student.worst}`
+}
+
+console.log(`Top 3 étudiant
+1: ${formatStudent(students[0])}
+2: ${formatStudent(students[1])}
+3: ${formatStudent(students[2])}
+`)
+
+//Créer un système capable de calculer la fréquence des mots dans une chaine de caractère
+
+const phrase = "Vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres."
+const frequencies = {}
+const words = phrase
+.toLowerCase()
+.replaceAll(',', '')
+.replaceAll('?', '')
+.replaceAll(':', '')
+.replaceAll('!', '')
+.split(' ')
+
+for(let word of words) {
+    if(frequencies[word]){
+        frequencies[word]++
+    } else {
+        frequencies[word] = 1
+    }
+}
+
+//Créer un tableau en le triant en fonction des mots qui appraissent le plus de fois
+
+const frequenciesArray = []
+for(let k in frequencies) {
+    frequenciesArray.push({
+        word: k,
+        count: frequencies[k]
+    })
+}
+frequenciesArray.sort((a, b) => b.count - a.count)
+console.log(frequenciesArray)
